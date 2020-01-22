b3561066ccf343245801e8c6476dd619cb192d17
YusukeKiyooka <s17c1045kh@s.chibakoudai.jp>
2020-01-22 18:29:51 +0900

    fix

---
+++ twice.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+def cb(message):
+    rospy.loginfo(message.data)
+
+if __name__ == '__main__':
+    rospy.init_node('twice')
+    sub = rospy.Subscriber('count_up', String, cb)
+    rospy.spin()
